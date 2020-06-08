ecdcc23ef8ef7ec1daacde739201df7ed6e77e49
Tobias Golbs <golbs@e-spirit.com>
2020-06-08 21:25:25 +0200

    Add new dependencies and remoe FSXAConfigProvider

--- pages/_.tsx
+++ pages/_.tsx
@@ -1,6 +1,6 @@
 import 'vue-tsx-support/enable-check'
 import { Component } from 'vue-property-decorator'
-import { FSXAPage, FSXAConfigProvider } from 'fsxa-pattern-library'
+import { FSXAPage } from 'fsxa-pattern-library'
 import * as tsx from 'vue-tsx-support'
 import './../assets/css/global.css'
 
@@ -10,12 +10,10 @@ export default class IndexPage extends tsx.Component<{}> {
     const path = this.$router.currentRoute.path
     return (
       <div class="w-full">
-        <FSXAConfigProvider>
-          <FSXAPage
-            path={path}
-            handleRouteChange={(route) => this.$router.push({ path: route })}
-          />
-        </FSXAConfigProvider>
+        <FSXAPage
+          path={path}
+          handleRouteChange={(route) => this.$router.push({ path: route })}
+        />
       </div>
     )
   }
