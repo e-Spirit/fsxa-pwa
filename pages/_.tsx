782fe36b171f0032150234419fdbf8b4b754da81
Tobias Golbs <golbs@e-spirit.com>
2020-06-08 09:05:28 +0200

    Add current state

--- pages/_.tsx
+++ pages/_.tsx
@@ -12,11 +12,7 @@ export default class IndexPage extends tsx.Component<{}> {
     return (
       <div class="w-full">
         <FSXAConfigProvider editMode debugMode>
-          <FSXAPage
-            id="c8a158a3-2ba3-427c-a7e4-7d41d9844464"
-            path={path}
-            handleRouteChange={console.log}
-          />
+          <FSXAPage path={path} handleRouteChange={route => this.$router.push({ path: route })} />
         </FSXAConfigProvider>
       </div>
     )
--- store/fsxa.ts
+++ store/fsxa.ts
@@ -9,10 +9,9 @@ const { getters, mutations, actions, state } = getFSXAModule(
     remotes: {
       fragments: 'cd8fa9b9-f278-4750-92e5-2e4fc7f0eea2'
     },
-    caas:
-      'https://caas.staging.delivery-platform.e-spirit.live/0b975076-5061-44e6-bbce-c1d9f73f6606/preview.content',
+    caas: 'https://caas.staging.delivery-platform.e-spirit.live',
     navigationService:
-      'https://do-caas-core02.navigation.prod.delivery-platform.e-spirit.live/navigation/preview.0b975076-5061-44e6-bbce-c1d9f73f6606',
+      'https://do-caas-core02.navigation.prod.delivery-platform.e-spirit.live/navigation',
     locale: 'de_DE'
   })
 )
