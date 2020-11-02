40d98e4bb3f20b6bb92be16a839fdcaf80f9511b
Tobias Golbs <golbs@e-spirit.com>
2020-11-02 10:06:57 +0100

    Add new dependencies

--- fsxa.config.ts
+++ fsxa.config.ts
@@ -1,5 +1,9 @@
 export default {
   devMode: true,
   sections: '~/components/fsxa/sections',
-  layouts: '~/components/fsxa/layouts'
+  layouts: '~/components/fsxa/layouts',
+  mapDataQuery: (query) => {
+    console.log(query)
+    return []
+  }
 }
