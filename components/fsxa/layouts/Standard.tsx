83609a9bde59fea48cd00b479d2432ec067344d5
Emre Izgoel <izgoel@e-spirit.com>
2020-11-26 11:44:15 +0100

    integrate-default-components 🖥️wip: TNG-824 updated standard layout

--- components/fsxa/layouts/Standard.tsx
+++ components/fsxa/layouts/Standard.tsx
@@ -1,27 +1,63 @@
 import Component from "vue-class-component";
 import { FSXABaseLayout } from "fsxa-pattern-library";
 import { Layouts } from "fsxa-ui";
-import HeaderSection, { Payload as HeaderSectionPayload } from "../sections/Header";
-import { Image } from "fsxa-api";
+import { FSXAGetters } from "fsxa-pattern-library";
+import { NavigationData, Image } from "fsxa-api";
+import { Sections } from "fsxa-ui";
+
+interface HeaderSectionPayload {
+  pt_text: string;
+  pt_picture?: Image;
+  pageId: string;
+}
 
 @Component({
   name: "FSXAStandardLayout",
 })
 class StandardLayout extends FSXABaseLayout<HeaderSectionPayload> {
+  get navigationData(): NavigationData {
+    return this.$store.getters[FSXAGetters.navigationData];
+  }
+  renderHeader() {
+    const currentPage = this.navigationData.idMap[this.data.pageId];
+    return (
+      <Sections.HeaderSection
+        title={this.data.pt_text}
+        breadcrumbs={
+          currentPage
+            ? currentPage.parentIds.map(parentId => {
+                const page = this.navigationData.idMap[parentId];
+                return {
+                  label: page.label || "",
+                  path: page.seoRoute,
+                  referenceId: parentId,
+                  referenceType: "PageRef",
+                };
+              })
+            : []
+        }
+        backgroundImage={
+          this.data.pt_picture?.resolutions.ORIGINAL
+            ? {
+                src: this.data.pt_picture.resolutions.ORIGINAL.url,
+                dimensions: {
+                  width: this.data.pt_picture.resolutions.ORIGINAL.width,
+                  height: this.data.pt_picture.resolutions.ORIGINAL.height,
+                },
+              }
+            : undefined
+        }
+        handleItemClick={(item: any) =>
+          this.handleRouteChangeRequest({ pageId: item.referenceId })
+        }
+      />
+    );
+  }
   render() {
-    console.log("MEINS!")
     if (this.content.length === 0) return null;
     return (
       <Layouts.SingleColumnLayout data-preview-id={this.content[0].previewId}>
-        <HeaderSection
-          payload={{
-            pt_picture: this.data.pt_picture,
-            pt_text: this.data.pt_text,
-            pageId: this.pageId,
-          }}
-          content={[]}
-        />
-        <h1>MEINS!</h1>
+        {this.renderHeader()}
         {this.content.length > 0 && this.renderContentElements(0)}
       </Layouts.SingleColumnLayout>
     );
