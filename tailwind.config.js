f87faaa09923205342901e22c20b8eee6fda4154
Tobias Golbs <golbs@e-spirit.com>
2021-01-20 14:59:12 +0100

    fix(tailwindcss): configure purgecss to keep used classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
    * We will prefix tailwind classes with tw- so we do not have any naming clashes with the fsxa-ui
    */
   prefix: 'tw-',
-  purge: [],
+  purge: {
+    content: [
+      './pages/**/*.vue',
+      './pages/**/*.tsx',
+      './pages/**/*.jsx',
+      './components/**/*.vue',
+      './components/**/*.tsx',
+      './components/**/*.jsx',
+      './assets/**/*.css'
+    ]
+  },
   theme: {
     extend: {
       colors: {
