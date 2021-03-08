1bc7439ca8e4e66fa47a1c8ed823a29cab055d3c
Vlado Plaga <plaga@e-spirit.com>
2021-03-08 13:08:02 +0100

    Embed a youtube video without using a video section from FSXA-UI - DRAFT, two versions

--- components/fsxa/sections/InteractiveYoutubeVideo.tsx
+++ components/fsxa/sections/InteractiveYoutubeVideo.tsx
@@ -2,7 +2,7 @@ import { Component } from 'vue-property-decorator'
 
 import { RichTextElement } from 'fsxa-api'
 import { FSXABaseSection } from 'fsxa-pattern-library'
-import { Sections } from 'fsxa-ui'
+import { Headline, Container, LineSeparator, TeaserSection } from 'fsxa-ui'
 
 export interface Payload {
   st_autoPlay: boolean
@@ -41,14 +41,60 @@ export interface Meta {
 class InteractiveYouTubeVideo extends FSXABaseSection<Payload, Meta> {
 
   render() {
-    const videoParams = this.payload.st_autoPlay.valueOf() == true ? [] : [{ param: "autoplay", value: "1" }];
+    const youtubeID = this.payload.st_youtubeVideo.value[0].identifier;
+    const autoplayParam = this.payload.st_autoPlay.valueOf() == true ? "?autoplay=true" : "";
+    const title = this.payload.st_youtubeVideo.value[0].value.title;
+    const description = this.payload.st_youtubeVideo.value[0].value.description;
+    const previewPicture = this.payload.st_youtubeVideo.value[0].value.thumbnailUrl;
+    const url = "https://www.youtube-nocookie.com/embed/"
     return (
-      <Sections.VideoSection
-        youtubeId={this.payload.st_youtubeVideo.value[0].identifier}
-        description={this.payload.st_youtubeVideo.value[0].value.description}
-        title={this.payload.st_youtubeVideo.value[0].value.title}
-        parameters={videoParams}
-      />
+
+      <div
+        data-testid="fsxa-video-section">
+
+        <Container><Headline>Most simple video section:</Headline></Container>
+
+        <Container>
+          {title && <Headline>{title}</Headline>}
+          {title && <LineSeparator />}
+          <div class="aspect-w-16 aspect-h-9 my-3">
+            <iframe
+              class="m-auto"
+              src={`${url}${youtubeID}${autoplayParam}`}
+              frameborder="0"
+              allowfullscreen
+            />
+          </div>
+          {description && (
+            <div class="m-1 uppercase text-sm md:pr-6 lg:pr-8">{description}</div>
+          )}
+        </Container>
+
+        <Container><Headline>Alternative video section (using a TeaserSection):</Headline></Container>
+
+        <TeaserSection
+          headline={title}
+          kicker=""
+          text=""
+          media={{
+            type: "image",
+            src: previewPicture,
+            previewId: "1000",
+            alt: "Video thumbnail",
+          }}
+          scopedSlots={{
+            media: () => <div class="aspect-w-16 aspect-h-9"><iframe
+              class="m-auto"
+              src={`${url}${youtubeID}${autoplayParam}`}
+              frameborder="0"
+              allowfullscreen
+            /></div>,
+            text: () => (
+              <div class="uppercase text-sm md:pr-6 lg:pr-8">{description}</div>
+            ),
+          }}
+        />
+      </div>
     )
   }
 }
