857aca2f6e9ac45b994dd96cad814de0a3fd3493
Vlado Plaga <plaga@e-spirit.com>
2021-03-11 17:16:35 +0100

    TNG-733 review changes

--- components/fsxa/sections/InteractiveYoutubeVideo.tsx
+++ components/fsxa/sections/InteractiveYoutubeVideo.tsx
@@ -2,7 +2,7 @@ import { Component } from 'vue-property-decorator'
 
 import { RichTextElement } from 'fsxa-api'
 import { FSXABaseSection } from 'fsxa-pattern-library'
-import { Headline, Container, LineSeparator, TeaserSection } from 'fsxa-ui'
+import { Headline, Container, LineSeparator } from 'fsxa-ui'
 
 export interface Payload {
   st_autoPlay: boolean
@@ -13,9 +13,9 @@ export interface Payload {
     value: {}
   },
   st_youtubeVideo: {
-    fsType: string,
-    name: string,
-    dapType: string,
+    fsType: string
+    name: string
+    dapType: string
     value: [
       {
         fsType: string
@@ -42,32 +42,27 @@ class InteractiveYouTubeVideo extends FSXABaseSection<Payload, Meta> {
 
   render() {
     const youtubeID = this.payload.st_youtubeVideo.value[0].identifier;
-    const autoplayParam = this.payload.st_autoPlay.valueOf() == true ? "?autoplay=true" : "";
+    const autoplayParam = this.payload.st_autoPlay.valueOf() ? "?autoplay=true" : "";
     const title = this.payload.st_youtubeVideo.value[0].value.title;
     const description = this.payload.st_youtubeVideo.value[0].value.description;
     const url = "https://www.youtube-nocookie.com/embed/"
     return (
-
-      <div
-        data-testid="fsxa-video-section">
-
-        <Container>
-          {title && <Headline>{title}</Headline>}
-          {title && <LineSeparator />}
-          <div class="aspect-w-16 aspect-h-9 my-3">
+      <Container>
+        {title ? <Headline>{title}</Headline> : null}
+        {title ? <LineSeparator /> : null}
+        <div class="aspect-w-16 aspect-h-9 my-3">
+          {youtubeID ?
             <iframe
               class="m-auto"
               src={`${url}${youtubeID}${autoplayParam}`}
               frameborder="0"
               allowfullscreen
-            />
-          </div>
-          {description && (
-            <div class="m-1 uppercase text-sm md:pr-6 lg:pr-8">{description}</div>
-          )}
-        </Container>
-
-      </div>
+            /> : <div><i>No YouTube identifier provided.</i></div>}
+        </div>
+        {description ? (
+          <div class="m-1 uppercase text-sm md:pr-6 lg:pr-8">{description}</div>
+        ) : null}
+      </Container>
     )
   }
 }
